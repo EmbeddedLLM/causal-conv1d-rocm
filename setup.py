fa7d8f92a3534484f4fdc2508eab2085dcedee74
kfl <kuanfu.liu@embeddedllm.com>
2024-04-12 06:48:18 +0000

    clean up

--- setup.py
+++ setup.py
@@ -63,7 +63,6 @@ def get_cuda_bare_metal_version(cuda_dir):
     raw_output = subprocess.check_output(
         [cuda_dir + "/bin/nvcc", "-V"], universal_newlines=True
     )
-    return "0", "0"
     output = raw_output.split()
     release_idx = output.index("release") + 1
     bare_metal_version = parse(output[release_idx].split(",")[0])
